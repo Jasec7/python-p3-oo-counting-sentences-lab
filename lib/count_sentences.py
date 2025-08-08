50789c9a71e8656018012651b5a92e63f6a0e66c
Jasec <jasec_7@hotmail.com>
2025-08-08 02:40:34 -0700

    complete python p3 oo counting sentences lab

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,38 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+  def __init__(self, value = ""):
+    self.value = value
+
+  @property
+  def value(self):
+    return self._value
+  
+  @value.setter
+  def value(self, value):
+    if isinstance(value, str):
+        self._value = value
+    else:
+       print("The value must be a string.")
+
+    
+  def is_sentence(self):
+      return self.value.endswith(".")
+       
+
+  def is_question(self):
+     return self.value.endswith("?")
+
+  def is_exclamation(self):
+     return self.value.endswith("!")
+  
+  def count_sentences(self):
+     signs = self.value.replace("?","|").replace(".","|").replace("!","|")
+     sentence = signs.split("|")
+     
+     filtered = [n for n in sentence if n.strip() != ""]
+     return len(filtered)
+        
+     
+     
+     
